d4eebce3bf3f7727aa0c3e6000cfef7fc7ece71f
Linet <teweilinet@gmail.com>
2024-04-25 06:02:07 +0300

    props

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,17 @@
 import React from "react";
 import user from "../data/user";
+
 function About(props) {
+  const { github, linkedin } = user.links;
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{props.bio}</p>
+      {props.bio ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-     <h3>Links</h3>
-     <a href="">{props.links}</a>
-     
+      <h3>Links</h3>
+      <a href={github}>{github}</a>
+      <a href={linkedin}>{linkedin}</a>
     </div>
   );
 }
